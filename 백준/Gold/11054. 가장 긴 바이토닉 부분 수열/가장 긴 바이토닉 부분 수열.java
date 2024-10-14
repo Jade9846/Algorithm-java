eb4d57fa4986bcef66982f7c84d9a4c8e01e4f52
Jade9846 <101115635+Jade9846@users.noreply.github.com>
2024-10-15 04:29:28 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 132 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
-
 public class Main {
     private static int n;
     private static int[] dp_up;
@@ -27,21 +26,29 @@ public class Main {
             dp_down[i] = 1;
         }
 
-        int[] reverseArray = reverse(numList);
+//        int[] reverseArray = reverse(numList);
 
         for(int i = 1 ; i < n; i++){
             for(int j = 0; j <i; j++){
                 if(numList[i] > numList[j]){
                     dp_up[i] = Math.max(dp_up[i], dp_up[j]+1);
                 }
-                if(reverseArray[i] > reverseArray[j]){
+//                if(reverseArray[i] > reverseArray[j]){
+//                    dp_down[i] = Math.max(dp_down[i], dp_down[j]+1);
+//                }
+            }
+        }
+
+        for(int i = n-2 ; i >= 0; i--){
+            for(int j = i+1; j <n; j++){
+                if(numList[i] > numList[j]){
                     dp_down[i] = Math.max(dp_down[i], dp_down[j]+1);
                 }
             }
         }
 
-        dp_down = reverse(dp_down);
-        
+//        dp_down = reverse(dp_down);
+
         int result = 0;
         for(int i = 0; i <n ; i++){
             result = Math.max(result, dp_up[i] + dp_down[i]);
@@ -50,11 +57,11 @@ public class Main {
         System.out.println(result-1);
 
     }
-    public static int[] reverse(int[] array){
-        int[] result = new int[array.length];
-        for(int i= 0; i < array.length; i++){
-            result[i] = array[array.length - 1 - i];
-        }
-        return result;
-    }
+//    public static int[] reverse(int[] array){
+//        int[] result = new int[array.length];
+//        for(int i= 0; i < array.length; i++){
+//            result[i] = array[array.length - 1 - i];
+//        }
+//        return result;
+//    }
 }
