5a79d930413995f17cf28ff54bbafe989eb282a8
Jade9846 <101115635+Jade9846@users.noreply.github.com>
2024-11-02 23:59:13 +0900

    [Gold IV] Title: 부분합, Time: 252 ms, Memory: 24128 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, S;
+    static int[] numList;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        numList = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            numList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 1;
+        int end = 1;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        while (end <= N) {
+            sum += numList[end++];
+           
+            while (sum >= S) {
+                minLength = Math.min(minLength, end - start);
+                sum -= numList[start++];
+            }
+        }
+        System.out.println(minLength == Integer.MAX_VALUE ? 0 : minLength);
+    }
+}
